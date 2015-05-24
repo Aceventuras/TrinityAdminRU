2294ab0c3bf32e000f4b81e14114d66ee41d9d1f
Aceventuras <Aceventuras@users.noreply.github.com>
2015-05-24 18:14:06 +0300

    Update TrinityAdmin.lua

--- TrinityAdmin.lua
+++ TrinityAdmin.lua
@@ -3,6 +3,8 @@
 -- TrinityAdmin Version 3.x
 -- TrinityAdmin is a derivative of MangAdmin.
 --
+-- Copyright (C) 2015 ruRU Aceventuras
+-- репозиторий https://github.com/Aceventuras/TrinityAdminRU
 -- Copyright (C) 2007 Free Software Foundation, Inc.
 -- License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>
 -- This is free software: you are free to change and redistribute it.
